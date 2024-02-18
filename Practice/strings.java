fe2c08a4dbb848a16c01fc344a5eb9d06737464f
akshad Gawde <gawdeakshad@gmail.com>
2024-02-18 23:36:31 +0530

    Strings

---
+++ Practice/strings.java
@@ -0,0 +1,8 @@
+class strings {
+    public static void main(String[] args) {
+
+        String name = "hello Akshad";
+        String lastName = " Gawde";
+        System.out.println(name + lastName);
+    }
+}
